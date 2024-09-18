54ef8d6785a77d8d64a103a58cc375b3bb23f368
Sweety-deeps <deepszweety5@gmail.com>
2024-09-18 22:33:29 +0530

    Create CsvHelperTests.cs

---
+++ test/cdtransformation.tests/Helpers/CsvHelperTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using cdtransformation.Helpers;
+using Xunit;
+
+namespace cdtransformation.tests.Helpers
+{
+	public class CsvHelperTests
+	{
+		[Fact]
+		public void Validate_DataTableWhenPassedCsvDataAsString()
+		{
+			var csvHelper = new CsvHelper();
+
+			string csvContent = "id,name\r\n123,mohan\r\n1234,testdata\r\n\r\n";
+
+			var result = csvHelper.GetDataFromCsv(false, csvContent);
+		}
+	}
+}
